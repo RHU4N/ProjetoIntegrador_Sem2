69ba1e6a0a196ce8249f8ff2e100094c04212100
RHU4N <rhuan3003@gmail.com>
2024-11-12 18:32:02 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <input type="email" placeholder='Email' />
+        <br />
+        <input type="password" placeholder='Senha' />
+        <br />
+        <input type="submit" value="Entrar" /> 
+      </div>
+    </>
+  )
+}
+
+export default App
